30a501ae1ff1bde9de94b5c77bd3e8e9925551d8
Michael A. MacDonald <mars@antlersoft.com>
2019-04-27 12:05:00 -0400

    Update logcat for more recent Android and missing debug logs

--- mobile/src/main/java/com/example/android/uamp/utils/LogHelper.java
+++ mobile/src/main/java/com/example/android/uamp/utils/LogHelper.java
@@ -94,7 +94,7 @@ public class LogHelper {
     }
 
     public static void log(String tag, int level, Throwable t, Object... messages) {
-        if (Log.isLoggable(tag, level)) {
+        if (BuildConfig.DEBUG || Log.isLoggable(tag, level)) {
             String message;
             if (t == null && messages != null && messages.length == 1) {
                 // handle this common case without the extra cost of creating a stringbuffer:
